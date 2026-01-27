96fc4e043ca88391dc9b229e6fa10da13045c793
Temurbek43 <seyitkomilovtemur@gmail.com>
2026-01-27 17:38:41 +0500

    function

--- app.js
+++ app.js
@@ -97,3 +97,52 @@
 //   sum = sum + a * Math.cos(h) - Math.sin(h ** 2);
 // }
 // console.log(sum);
+
+function faqtarial(n) {
+  let sm = 1;
+  for (let i = 1; i <= n; i++) {
+    sm *= i;
+  }
+  return sm;
+}
+
+let n = 2;
+let x = 1;
+
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+  sum += x ** i / faqtarial(i);
+}
+console.log(sum);
+
+// function declaratins
+salom();
+function salom() {
+  console.log("Assalomu aleykum");
+}
+// function expression
+let alik = function () {
+  console.log("Va aleykum assalom");
+};
+alik();
+
+// arrow function
+const hi = () => {
+  console.log("hi");
+};
+hi();
+
+function birthDate(currY, age) {
+  if (typeof age == "number" && typeof currY == "number") {
+    console.log(currY - age);
+  } else {
+    console.log("invalid data");
+  }
+}
+
+birthDate(2026, 16);
+
+let arifmetik = function (a, b, c) {
+  console.log((a + b + c) / 3);
+};
+arifmetik(33, 28, 17);
